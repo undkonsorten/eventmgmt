a0768a59df0d447197a781709891f9a7099c3221
Eike Starkmann <starkmann@undkonsorten.com>
2013-09-19 12:29:37 +0200

    ABNAHME - task 4102: BE Plugin erstellen https://support.undkonsorten.com/issues/show/4102

--- ext_tables.php
+++ ext_tables.php
@@ -5,9 +5,14 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	$_EXTKEY,
-	'Calendar',
+	'List',
 	'Calendar'
 );
+
+$pluginSignature = str_replace('_','',$_EXTKEY) . '_list';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
+
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Calendar');
 
@@ -50,7 +55,7 @@ $TCA['tx_event_domain_model_event'] = array(
 		'crdate' => 'crdate',
 		'cruser_id' => 'cruser_id',
 		'dividers2tabs' => TRUE,
-		'sortby' => 'sorting',
+
 		'versioningWS' => 2,
 		'versioning_followPages' => TRUE,
 		'origUid' => 't3_origuid',
@@ -63,7 +68,7 @@ $TCA['tx_event_domain_model_event'] = array(
 			'starttime' => 'starttime',
 			'endtime' => 'endtime',
 		),
-		'searchFields' => 'title,subtitle,short_title,teaser,description,image,files,start,end,all_day,fee,view,category,primary_calendar,secundary_calendar,register,link,location,organizer,',
+		'searchFields' => 'title,subtitle,short_title,teaser,description,image,files,start,end,all_day,fee,primary_calendar,secundary_calendar,register,link,location,organizer,',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Event.php',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_event_domain_model_event.gif'
 	),
@@ -97,20 +102,6 @@ $TCA['tx_event_domain_model_link'] = array(
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_event_domain_model_link.gif'
 	),
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-		$_EXTKEY,
-		'tx_event_domain_model_event',
-
-		// optional: in case the field would need a different name as "categories".
-		// The field is mandatory for TCEmain to work internally.
-		$fieldName = 'categories',
-
-		// optional: add TCA options which controls how the field is displayed. e.g position and name of the category tree.
-		$options = array()
-);
-
-
 
 $tmp_event_columns = array(
 
