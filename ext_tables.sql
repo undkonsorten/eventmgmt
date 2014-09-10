0d0e996e38c191df1fd778cb238f4ee64b5f992d
eike.starkmann <devnull@localhost>
2014-09-10 14:38:58 +0200

    FIX: wrong sql field event -> events

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE tx_eventmgmt_domain_model_calendar (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
-	event int(11) unsigned DEFAULT '0' NOT NULL,
+	events int(11) unsigned DEFAULT '0' NOT NULL,
 
 	name varchar(255) DEFAULT '' NOT NULL,
 	single_pid int(11) unsigned DEFAULT '0' NOT NULL,
