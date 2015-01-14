dc13128ea7fae6994282014e8c01c92d60de6897
Eike Starkmann <starkmann@undkonsorten.com>
2015-01-14 14:35:40 +0100

    ADD: extbasetyp to sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -72,6 +72,7 @@ CREATE TABLE tx_eventmgmt_domain_model_event (
 	contact int(11) unsigned DEFAULT '0',
 	display int(11) unsigned DEFAULT '0' NOT NULL,
 	category int(11) unsigned DEFAULT '0' NOT NULL,
+	tx_extbase_type varchar(255) DEFAULT '' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
