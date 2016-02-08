d0415fff6773133c3c1ccdd5cbfe6794d37edcc0
eike.starkmann <eike.starkmann@undkonsorten.com>
2016-02-08 14:39:32 +0100

    ADD: Sorting for location

---
+++ Classes/Utility/EventLocations.php
@@ -0,0 +1,43 @@
+<?php
+namespace Undkonsorten\Eventmgmt\Utility;
+
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Felix Althaus <felix.althaus@undkonsorten.com>, undkonsorten
+ *  Eike Starkmann <eike.starkmann@undkonsorten.com>, undkonsorten
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class EventLocations
+{
+    public function getLocations($config)
+    {
+        $itemNames = array_map(function($item) {
+            return $item[0];
+        }, $config['items']);
+        array_multisort($itemNames, $config['items']);
+       
+        return $config;
+    }
+}
