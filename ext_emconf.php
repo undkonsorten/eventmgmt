978d372823a64b92eeb76edd40be2a74a3396dde
karsten <nowak@undkonsorten.com>
2019-08-16 16:49:12 +0200

    [TASK] Initial TYPO3 9.5 compatibility
    Related: #17089

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '5.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.7.99',
+			'typo3' => '9.5.0-9.5.99',
 			'addressmgmt' => '3.1.1-3.99.99',
 		),
 		'conflicts' => array(
